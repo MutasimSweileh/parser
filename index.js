835a438c95de4d3dba779d9fea8839b39d777101
Mutasim Sweileh <mutasimsweileh@gmail.com>
2024-09-01 00:43:18 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import Parser from '@postlight/parser';
+//import Parser from '@postlight/parser';
+import Parser from '@postlight/mercury-parser';
 import logger from 'logops';
 import morgan from 'morgan';
+
+import { Readability } from "@mozilla/readability"
+import { JSDOM, VirtualConsole } from "jsdom"
 
 const app = express()
 
@@ -9,8 +13,8 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 //app.use(expressLogging(logger));
-app.use(morgan("combined"));
 app.listen(port, () => console.log(`App is listening on port ${port}.`));
+app.use(morgan("combined"));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
@@ -30,16 +34,48 @@ let isObjectEmpty = (object) => {
     return !object || Object.keys(object).length === 0;
 }
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// })
+async function get_html(url) {
+    let response = await JSDOM.fromURL(url);
+    return response.serialize()
+}
 app.all('/parser', async function (req, res, next) {
-    const { url, html, contentType = "html" } = (!isObjectEmpty(req.body) ? req.body : req.query);
-    let r = await Parser.parse(url, { contentType, html });
+    let { url, html, contentType = "html", type = "readability" } = (!isObjectEmpty(req.body) ? req.body : req.query);
+    let r = null;
+    try {
+        if (!html)
+            html = await get_html(url)
+        if (type == "postlight") {
+            r = await Parser.parse(url, { contentType, html });
+            if (r.error)
+                throw new Error(r.message);
+        } else {
+            const doc = new JSDOM(html, {
+                url
+            }).window.document;
+            const reader = new Readability(doc, { debug: false });
+            r = reader.parse()
+            if (!r || !r.textContent) {
+                throw new Error("Could not parse the page.");
+            }
+            r.word_count = countWords(r.textContent);
+        }
+        r = { "success": true, "data": r };
+    } catch (error) {
+        error = (!isString(error) ? { "message": error.message, "code": error.code } : { "message": error });
+        r = { "success": false, ...error };
+    }
     return res.send(r);
 })
-
+function countWords(s) {
+    s = s.replace(/(^\s*)|(\s*$)/gi, "");//exclude  start and end white-space
+    s = s.replace(/[ ]{2,}/gi, " ");//2 or more space to 1
+    s = s.replace(/\n /, "\n"); // exclude newline with a start spacing
+    return s.split(' ').filter(function (str) { return str != ""; }).length;
+    //return s.split(' ').filter(String).length; - this can also be used
+}
 function isString(value) {
     return typeof value === 'string' || value instanceof String;
 }
